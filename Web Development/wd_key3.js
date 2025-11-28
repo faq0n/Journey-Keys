d34b3d5e23e89a18ef03e57a6ea23b938c852e43
faq0n <synqun@disroot.org>
2025-11-28 23:53:20 +0100

    solve key 3

--- Web Development/wd_key3.js
+++ Web Development/wd_key3.js
@@ -13,29 +13,29 @@ const name = "your_email_here";
 // === TASK 1 ===
 // Return a React component string that displays "Welcome <name>"
 function createComponent(name) {
-    // TODO
-    return "";
+        //REACTDom.createRoot('root').render(`<h1>Welcome ${name}</h1>`);
+        return `function Welcome() { return <h1>Welcome ${name}</h1>; }`;
 }
 
 // === TASK 2 ===
 // Return a simple JSX Route string like: "<Route path='/' element={<Home />} />"
 function createRoute() {
     // TODO
-    return "";
+    return "<Route path='/' element={<Home />} />";
 }
 
 // === TASK 3 ===
 // Return a React state initialization code: "const [count, setCount] = useState(0);"
 function useStateSnippet() {
     // TODO
-    return "";
+    return "const [count, setCount] = useState(0);"
 }
 
 // === TASK 4 ===
 // Return a Bootstrap button JSX: "<button class='btn btn-primary'>Click Me</button>"
 function bootstrapButton() {
     // TODO
-    return "";
+    return "<button class='btn btn-primary'>Click Me</button>";
 }
 
 module.exports = { name, createComponent, createRoute, useStateSnippet, bootstrapButton };
